8566b989d4cecacbbc0852cd51c27810cfb402ff
Vishalghost <vishal.im05@gmail.com>
2025-07-23 23:47:04 +0530

    Add install batch file

---
+++ install_devwizard.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Installing DevWizard...
+pip install -e .
+echo.
+echo DevWizard installed successfully!
+echo You can now run it by typing 'devwizard' in your terminal.
+echo.
+pause
